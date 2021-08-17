351016ed26661e58100414ca3771bb0055abec04
Samuel <Samuelpmontague@gmail.com>
2021-08-17 16:43:28 -0500

    updated views folder

---
+++ public/js/sinup.js
@@ -0,0 +1,23 @@
+const signupFormHandler = async (e) => {
+    e.preventDefault();
+  
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if(name && email && password){
+      const res = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({name, email, password}),
+        headers: {'Content-Type': 'application/json'}
+      });
+  
+      if(res.ok){
+        document.location.replace('/dashboard');
+      } else {
+        alert(res.statusText);
+      }
+    }
+  };
+  
+  document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
